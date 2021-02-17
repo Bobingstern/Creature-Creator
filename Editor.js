365b4d016fd40b47716170e244a893c978a08f23
Bob Boi <akgaming1322@gmail.com>
2021-02-17 16:07:04 -0500

    Drawing Modes

--- Editor.js
+++ Editor.js
@@ -1,9 +1,10 @@
 class Editor {
   constructor() {
+    //------Rectangluar Control
     this.RectDraw = false
     this.RectDrawX
     this.RectDrawY
-    this.RectDrawing = false
+    this.RectDrawing = true
     this.rectDrawn = false
     this.id = 0
     this.world = new b2World(new b2Vec2(0, 50))
@@ -15,45 +16,60 @@ class Editor {
     this.UndoButton.mousePressed(this.ridLastPieceOfShit)
     this.ClearButton = this.makeButton("Clear World", [255, 0, 0], 210, 10, 100, 50, 19)
     this.ClearButton.mousePressed(this.ridWorldOfShit)
+    this.ToggleDrawModeButton = this.makeButton("Switch to Joint mode", [0, 127, 255], 10, 60, 100, 50, 14)
+    this.ToggleDrawModeButton.mousePressed(this.toggleMode)
     this.drawMode = true
+    //------------------------------Joint Control
+    this.jointDraw = false
+    this.jointBody = []
+
+    //-----------------------------------------
+
+
     //----Ground
     this.bodies.push(makeBox(this.world, b2Body.b2_staticBody, 0, height, width, 20, 1, 0.3, 0.1, 1))
     this.bodyScales.push([width, 20])
+    //---
   }
 
   show() {
-    if (this.RectDraw) {
-      push()
-      fill(255, 255, 255, 50)
-      //rectMode(CENTER)
-      rect(this.RectDrawX, this.RectDrawY, (mouseX - this.RectDrawX), (mouseY - this.RectDrawY))
-      pop()
-    } else {
-      this.RectDrawX = null
-      this.RectDrawY = null
-    }
-    if (this.drawMode) {
-      for (let i = 0; i < bodyData.length; i++) {
+    //--------------------Rect Stuff
+      if (this.RectDraw) {
         push()
         fill(255, 255, 255, 50)
         //rectMode(CENTER)
-        rect(bodyData[i][0], bodyData[i][1], bodyData[i][2], bodyData[i][3])
+        rect(this.RectDrawX, this.RectDrawY, (mouseX - this.RectDrawX), (mouseY - this.RectDrawY))
         pop()
+      } else {
+        this.RectDrawX = null
+        this.RectDrawY = null
       }
-    } else {
-      for (let i = 0; i < this.bodies.length; i++) {
-        let pos = this.bodies[i].GetPosition()
-        let angle = this.bodies[i].GetAngle()
-        push()
-        fill(255, 255, 255, 100)
-        translate(pos.x*SCALE, pos.y*SCALE)
-        rectMode(CENTER)
-        rotate(angle)
+      if (this.drawMode) {
+        for (let i = 0; i < bodyData.length; i++) {
+          push()
+          fill(255, 255, 255, 50)
+          //rectMode(CENTER)
+          rect(bodyData[i][0], bodyData[i][1], bodyData[i][2], bodyData[i][3])
+          pop()
+        }
+      } else {
+        for (let i = 0; i < this.bodies.length; i++) {
+          let pos = this.bodies[i].GetPosition()
+          let angle = this.bodies[i].GetAngle()
+          push()
+          fill(255, 255, 255, 100)
+          translate(pos.x*SCALE, pos.y*SCALE)
+          rectMode(CENTER)
+          rotate(angle)
 
-        rect(0, 0, this.bodyScales[i][0]*2, this.bodyScales[i][1]*2)
-        pop()
+          rect(0, 0, this.bodyScales[i][0]*2, this.bodyScales[i][1]*2)
+          pop()
+        }
       }
-    }
+      //-------------------------------Joint stuff
+
+
+      //----------------------------------------
   }
 
   addShitToWorld() {
@@ -79,20 +95,33 @@ class Editor {
     if (editor.bodyScales.length < 1) {
       return
     }
-    let backupBodies = editor.bodies.slice(0, -1)
-    let backupBodyScales = editor.bodyScales.slice(0, -1)
-    let backupBodyData = bodyData.slice(0, -1)
-    editor.ridWorldOfShit()
-    editor.bodies = backupBodies
-    editor.bodyScales = backupBodyScales
-    bodyData = backupBodyData
-    // editor.bodies = editor.bodies.slice(0, -1)
-    // editor.bodyScales = editor.bodyScales.slice(0, -1)
-    // bodyData = bodyData.slice(0, -1)
+    // let backupBodies = editor.bodies.slice(0, -1)
+    // let backupBodyScales = editor.bodyScales.slice(0, -1)
+    // let backupBodyData = bodyData.slice(0, -1)
+    // editor.ridWorldOfShit()
+    // editor.bodies = backupBodies
+    // editor.bodyScales = backupBodyScales
+    // bodyData = backupBodyData
+    editor.bodies = editor.bodies.slice(0, -1)
+    editor.bodyScales = editor.bodyScales.slice(0, -1)
+    bodyData = bodyData.slice(0, -1)
   }
 
-  setRectMode() {
-    editor.RectDraw = true
+  toggleMode() {
+    editor.ToggleDrawModeButton.remove()
+    // if (!(editor.drawMode)) {
+    //   editor.ToggleDrawModeButton = editor.makeButton("In Simulation Mode!", [127, 0, 0], 10, 60, 100, 50, 14)
+    //   editor.ToggleDrawModeButton.mousePressed(editor.toggleMode)
+    //   return
+    // }
+    if (editor.RectDrawing) {
+      editor.ToggleDrawModeButton = editor.makeButton("Switch to Joint Mode", [0, 127, 255], 10, 60, 100, 50, 14)
+    } else {
+      editor.ToggleDrawModeButton = editor.makeButton("Switch to Rect Mode", [0, 127, 255], 10, 60, 100, 50, 14)
+    }
+    editor.ToggleDrawModeButton.mousePressed(editor.toggleMode)
+    editor.RectDrawing = !(editor.RectDrawing)
+    editor.jointDraw = !(editor.jointDraw)
   }
 
   update() {
@@ -123,12 +152,31 @@ function mouseClicked() {
     editor.id++
     console.log(bodyData)
   }
+  //----Joint
+  if (editor.jointDraw) {
+    let closest = 0
+    let closestDist = 1000000000000
+    for (let i = 0; i < bodyData.length; i++) {
+      let d = dist(bodyData[i][0] + bodyData[i][2] / 2, bodyData[i][1] + bodyData[i][3] / 2, mouseX, mouseY)
+      if (d < closestDist) {
+        closestDist = d
+        closest = i
+      }
+    }
+
+    push()
+    fill(255, 255, 255)
+    rect(bodyData[closest][0] + bodyData[closest][2] / 2, bodyData[closest][1] + bodyData[closest][3] / 2, 50, 50)
+    pop()
+  }
 }
 
 function keyPressed() {
-  if (keyCode === 82) {
-    editor.RectDraw = true
-    editor.RectDrawX = mouseX
-    editor.RectDrawY = mouseY
+  if (editor.RectDrawing){
+    if (keyCode === 82) {
+      editor.RectDraw = true
+      editor.RectDrawX = mouseX
+      editor.RectDrawY = mouseY
+    }
   }
 }
